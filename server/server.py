5b26f1c191a235e7c86144892df57cb48ee78351
BatelCo <batel.cohen100@gmail.com>
2022-11-16 22:28:57 +0200

    add router

--- server/server.py
+++ server/server.py
@@ -19,7 +19,7 @@ app.add_middleware(
 )
 
 app.include_router(transactions.router)
-# app.include_router(breakdown.router)
+app.include_router(breakdown.router)
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
