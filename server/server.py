379cc38a0a1dcafa7c9c1f069338fcc946a25e55
BatelCo <batel.cohen100@gmail.com>
2022-11-16 16:38:56 +0200

    add server origins

--- server/server.py
+++ server/server.py
@@ -1,12 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
-from routers import breakdown, transoctions
+from routers import breakdown, transactions
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.include_router(transoctions.router)
-app.include_router(breakdown.router)
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(transactions.router)
+# app.include_router(breakdown.router)
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
