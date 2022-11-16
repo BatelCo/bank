09caa7414ae1331c950704f632787fd90faef24a
BatelCo <batel.cohen100@gmail.com>
2022-11-16 22:29:24 +0200

    naming chages

--- server/models/queries.py
+++ server/models/queries.py
@@ -1,20 +1,20 @@
-select_all_transactions =    """
-                                SELECT *
-                                From transactions
-                                """
+all_transactions = """
+                   SELECT *
+                   From transactions
+                   """
 
 insert_transaction =    """
                            INSERT into transactions (amount,category,vendor) 
                            values (%f,%s,%s)
                            """
                         
-sql_delete_transaction =    """
+delete_transaction =    """
                             DELETE FROM transactions 
                             WHERE id = %s
                             """
 
-sql_breakdown_transaction =     """
-                                SELECT transactions.category , SUM(transactions.amount) AS total_amount
-                                From transactions
-                                GROUP BY transactions.category
-                                """
+breakdown_by_category = """
+                            SELECT transactions.category , SUM(transactions.amount) AS total_amount
+                            From transactions
+                            GROUP BY transactions.category
+                            """    
