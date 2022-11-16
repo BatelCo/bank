7a34c72f4e82e1587c53dd9bb35f8d119bf67eac
BatelCo <batel.cohen100@gmail.com>
2022-11-16 22:30:06 +0200

    add breakdown rout

--- server/routers/breakdown.py
+++ server/routers/breakdown.py
@@ -1,11 +1,16 @@
 from typing import List
-from fastapi import APIRouter
-from fastapi import status
+from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
+from models.data_manager import DataManager
+
 
 router = APIRouter()
+data_manager = DataManager()
+
 
 @router.get("/breakdown", status_code=status.HTTP_200_OK)
 async def get_breakdown():
-    return "breakdown"
-    
+    try:
+        return {"breakdown" : (data_manager.get_breakdown_from_db())}
+    except Exception as e:
+        return JSONResponse({"Error": e}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) 
