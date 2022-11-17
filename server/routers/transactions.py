9e3561a5f782de9476e254379764d56159604dbd
BatelCo <batel.cohen100@gmail.com>
2022-11-17 18:11:17 +0200

    delete route added

--- server/routers/transactions.py
+++ server/routers/transactions.py
@@ -3,20 +3,23 @@ from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 from models.data_manager import DataManager
 
-import inspect
-
 router = APIRouter()
 data_manager = DataManager()
 
-print(inspect.getmembers(DataManager, predicate=inspect.isfunction))
-
 @router.get("/transactions", status_code=status.HTTP_200_OK)
-async def get_transactions():
+def get_transactions():
     try:
-        return {"transactions":(data_manager.get_transactions_from_db())}
+        return JSONResponse({"transactions" : (data_manager.get_transactions_from_db())})
     except Exception as e:
         return JSONResponse({"Error": e}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) 
     
+@router.delete("/transactions/{id}", status_code=status.HTTP_202_ACCEPTED)
+def delete_transactions(id):
+    try:
+        data_manager.delete_transaction(int(id))
+        return {"deleted" : "true"}
+    except Exception as e:
+        return JSONResponse({"Error": e}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) 
 
 # @router.post("/transactions}", status_code=status.HTTP_200_OK)
 # async def insert_transaction():
