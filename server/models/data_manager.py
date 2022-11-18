9d4e87c159531298bae531abfa996a33c78d9758
BatelCo <batel.cohen100@gmail.com>
2022-11-18 13:14:01 +0200

    add delete transaction

--- server/models/data_manager.py
+++ server/models/data_manager.py
@@ -3,28 +3,43 @@ import pymysql
 
 
 class DataManager():
-        def __init__(self):
-            try:
-                self.connection = pymysql.connect(
-                                                    host='localhost',
-                                                    user='root',
-                                                    password="",
-                                                    db="bank_app",
-                                                    charset="utf8",
-                                                    cursorclass=pymysql.cursors.DictCursor
-                                                )
-            except pymysql.Error as e:
-                print("Error connecting to MySQL", e)
+    def __init__(self):
+        try:
+            self.connection = pymysql.connect(
+                host='localhost',
+                user='root',
+                password="",
+                db="bank_app",
+                charset="utf8",
+                cursorclass=pymysql.cursors.DictCursor
+                )
+        except pymysql.Error as e:
+            print("Error connecting to MySQL", e)
+
     
-        def get_transactions_from_db(self):
-            try:
-                with self.connection.cursor() as cursor:
-                    cursor.execute(queries.select_all_transactions)
-                    result = cursor.fetchall()
-                    return result
-            except pymysql.Error as e:
-                raise e
+    def get_transactions_from_db(self):
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(queries.all_transactions)
+                result = cursor.fetchall()
+                return result
+        except pymysql.Error as e:
+            raise e
 
+    def get_breakdown_from_db(self):
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(queries.breakdown_by_category)
+                result = cursor.fetchall()
+                return result
+        except pymysql.Error as e:
+            raise e
 
-# d_m = DataManager()
-# print(d_m.get_transactoins())
+    def delete_transaction(self, id):
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(queries.delete_transaction, [id])
+                self.connection.commit()
+        except pymysql.Error as e:
+            raise e
+
