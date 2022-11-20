aa09254210fb76d1f6c181756691fce3f7118729
BatelCo <batel.cohen100@gmail.com>
2022-11-20 17:10:06 +0200

    update transactions

--- client/src/components/Transactions/Transactions.js
+++ client/src/components/Transactions/Transactions.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import { useState, useEffect } from 'react'
 import Transaction from '../Transaction/Transaction';
-import axios from 'axios';
+import { BankApi } from '../../data/BankApi';
 
 export default function Transactions() {
-  const [transactions, setTransactions] = useState([])
+	const [transactions, setTransactions] = useState([])
 
-  useEffect(() => {
-    loadTransactions() 
-  },[])
-  
-async function deleteTransaction(id){
-    await axios.delete(`http://localhost:8000/transactions/${id}`)
-    loadTransactions() 
-  }
+	useEffect(() => {
+		loadTransactions() 
+	},[])
 
-  async function loadTransactions(){
-    let promise= await axios.get("http://localhost:8000/transactions")
-    let newTransactions = promise.data.transactions
-    setTransactions(newTransactions) 
-    console.log(transactions)
-  }
-  
-  return (
-    <div className='transactions-board'>{transactions.map(transaction =>
-         <Transaction key={transaction.id} transaction={transaction} deleteTransaction={deleteTransaction}/>)}
-    </div>
-  )
+	async function deleteTransaction(id){
+		await BankApi().deleteTransaction(id)
+		loadTransactions() 
+	}
+
+	async function loadTransactions(){
+		let newTransactions = await BankApi().getTransactions()
+		setTransactions(newTransactions.data.transactions)
+	}
+	
+	return (
+		<div className='transactions-board'>{transactions.map(transaction =>
+			<Transaction key={transaction.id} transaction={transaction} deleteTransaction={deleteTransaction}/>)}
+		</div>
+	)
 }
