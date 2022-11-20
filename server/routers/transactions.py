427b539f4234b4aefea33a796552cee67851b32a
BatelCo <batel.cohen100@gmail.com>
2022-11-20 18:17:10 +0200

    update transactions route

--- server/routers/transactions.py
+++ server/routers/transactions.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Request
 from fastapi.responses import JSONResponse
 from models.data_manager import DataManager
 
@@ -21,9 +21,12 @@ def delete_transactions(id):
     except Exception as e:
         return JSONResponse({"Error": e}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) 
 
-# @router.post("/transactions}", status_code=status.HTTP_200_OK)
-# async def insert_transaction():
+@router.post("/transactions", status_code=status.HTTP_201_CREATED)
+async def insert_transaction(request: Request):
+    try:
+        add_transaction_fields = await request.json()
+        data_manager.add_transaction(add_transaction_fields["amount"], add_transaction_fields["category"], add_transaction_fields["vendor"])
+        return{"transaction added" : "true"}
+    except Exception as e:
+        raise JSONResponse({"Error": e},status_code=status.HTTP_400_BAD_REQUEST)
 
-
-# @router.delete("/transactions}", status_code=status.HTTP_200_OK)
-# async def delete_transaction():
