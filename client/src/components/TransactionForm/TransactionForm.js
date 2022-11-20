c978e3c88e5d162ccc072d3a0606670b5f895aff
BatelCo <batel.cohen100@gmail.com>
2022-11-20 17:08:50 +0200

    add transaction form

---
+++ client/src/components/TransactionForm/TransactionForm.js
@@ -0,0 +1,58 @@
+import React, {useState, useEffect} from 'react';
+import "./TransactionForm.css"
+import axios from 'axios';
+import constants from '../../constants';
+
+export default function TransactionForm(props) {
+    const [transactionInputs, setTransactionInputs] = useState({amountInput:"", categoryInput:"", vendorInput:""})
+
+    const handleChange = e =>{
+        let newTransactionInputs={...transactionInputs}
+        newTransactionInputs[e.target.name] = e.target.value
+        console.log(newTransactionInputs)
+        setTransactionInputs(newTransactionInputs)
+    }
+
+    async function insertTransaction(sign){
+        console.log(sign)
+        await axios.post(`http://localhost:8000/transactions`,
+        {amount: (sign == "-") ? -transactionInputs.amountInput : transactionInputs.amountInput,
+        category: transactionInputs.categoryInput,
+        vendor: transactionInputs.vendorInput});
+    }
+
+    const updateBalance=()=>{
+        console.log(updateBalance)        
+    }
+
+    const validInput=()=>{
+        if (transactionInputs.amountInput > 0 && transactionInputs.categoryInput != "" && transactionInputs.vendorInput != ""){
+            return true
+        }
+        return false
+    }
+
+    const addTransaction= (sign) =>{
+        if (validInput()){
+            console.log("addTransaction")
+            insertTransaction(sign)
+            updateBalance()
+        }
+        else{
+            alert("Invalid input")
+        }
+    }
+
+    return (
+    <div>
+        <div className="title">Insert Transaction:</div>
+        <div className="transaction-form">
+            <input className="inputform" min='0' onChange={handleChange} placeholder='amount' name="amountInput" type="number"></input>
+            <input className="inputform" onChange={handleChange} placeholder='category' name="categoryInput" type="text"></input>
+            <input className="inputform" onChange={handleChange} placeholder='vendor' name="vendorInput" type="text"></input><br/>
+            <button className="deposit" name="deposit" onClick={()=>addTransaction(constants.signConstants.PLUS)}>Deposit</button>
+      <button className="withdraw" name="withdraw" onClick={()=>addTransaction(constants.signConstants.MINUS)}>Withdraw</button>
+        </div>
+    </div>
+  )
+}
