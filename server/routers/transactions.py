7fd564d4e932373be2a7ddd74b16f2f6acf13c39
BatelCo <batel.cohen100@gmail.com>
2022-11-21 23:28:07 +0200

    error handling update

--- server/routers/transactions.py
+++ server/routers/transactions.py
@@ -14,23 +14,25 @@ def get_transactions():
         return JSONResponse({"Error": str(e)}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) 
     
 @router.delete("/transactions/{id}", status_code=status.HTTP_202_ACCEPTED)
-def delete_transactions(id):
+def delete_transaction(id):
     try:
         data_manager.delete_transaction(int(id))
         return {"deleted" : "true"}
+    except data_manager.ElementNotExistError as e:
+        return JSONResponse({"Error": "Transaction does not exist"}, status_code=status.HTTP_404_NOT_FOUND)
+
     except Exception as e:
         return JSONResponse({"Error": e}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) 
 
 @router.post("/transactions", status_code=status.HTTP_201_CREATED)
 async def insert_transaction(request: Request):
     try:
-        add_transaction_fields = await request.json()
-        data_manager.add_transaction(add_transaction_fields["amount"],
-                                     add_transaction_fields["category"],
-                                     add_transaction_fields["vendor"])
-        return{"transaction added" : "true"}
+        transaction_data = await request.json()
+        new_transaction = data_manager.add_transaction(transaction_data)
+        return JSONResponse({"status": "Success. Added Transaction", "transaction":new_transaction},
+            status_code = status.HTTP_201_CREATED)
     except Exception as e:
-            return JSONResponse({"Error": e}, status_code=status.HTTP_400_BAD_REQUEST)
+            return JSONResponse({"Error": e}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.get("/transactions/balance", status_code=status.HTTP_200_OK)
 def get_balance():
@@ -46,7 +48,8 @@ async def update_balance(request: Request):
         current_balance = data_manager.get_balance_from_db()[0]["amount"]
         updated_balance = current_balance + value_to_update  
         data_manager.update_balance(updated_balance)
-        return{"balance updated" : "true"}
+        return JSONResponse({"status": "Success. Balance Updated", "balance":updated_balance},
+            status_code = status.HTTP_201_CREATED)
     except Exception as e:
         return JSONResponse({"Error":e}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) 
     
