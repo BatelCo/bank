29418d9dd493e3f797a4a590b4a396a6f1f5da6b
BatelCo <batel.cohen100@gmail.com>
2022-11-21 19:01:37 +0200

    update transactoin text

--- client/src/components/Transaction/Transaction.js
+++ client/src/components/Transaction/Transaction.js
@@ -6,12 +6,11 @@ export default function Transaction(props) {
         props.deleteTransaction(props.transaction.id, -props.transaction.amount)
       }
 
-
     return (
         <div className="transaction" key={props.transaction.id}>
-            <span className='category'>{props.transaction.category} | </span>
-            <span className='vendor'>{props.transaction.vendor} </span>
-            <span className='amount'>| {props.transaction.amount}₪</span>
+            <span className='category'>{props.transaction.category}</span>
+            <span className='vendor'>{props.transaction.vendor}</span>
+            <span className='amount'>{props.transaction.amount}₪</span>
             <span>
                 <button className="delete-btn" onClick={deleteTransaction}>
                 <span>Delete</span> 
