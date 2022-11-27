b1412e60ae18b47284d19e634c6645469d6a53ff
batelco <batel.cohen100@gmail.com>
2022-11-27 10:31:52 +0200

    update error handling

--- server/routers/transactions.py
+++ server/routers/transactions.py
@@ -20,9 +20,7 @@ def delete_transaction(id):
     except ElementNotExistError as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"{id} {str(e)}")
     except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(e))
-    
-    
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 @router.post("/transactions", status_code=status.HTTP_201_CREATED)
 async def insert_transaction(request: Request):
